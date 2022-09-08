0d2dd316aeeabf545b4e34c6b00bb641ab214fb7
Mitchell Lev Merka <lev@merka.us>
2022-09-07 18:25:31 -0700

    quick link fix

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,7 +4,10 @@ import React from 'react'
 
 const Contact = () => {
   return (
+    <div>
     <a href="mailto: lev@merka.us">Shoot Me an E-mail!</a>
+    <a href="https://prometheus-flame.herokuapp.com/>href=">projects</a>
+    </div>
   )
 }
 

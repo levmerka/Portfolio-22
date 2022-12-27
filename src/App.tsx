86cebe718f30d027f433a737cdefa4c77f61750b
Mitchell Lev Merka <lev@merka.us>
2022-12-27 14:26:43 -0800

    links and text interactivity

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import Footer from "./pages/Footer";
 import Resume from "./pages/Resume";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 function App() {
   return (
